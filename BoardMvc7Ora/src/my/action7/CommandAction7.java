e246f2ae92289b844c91f694417b16d865472c5e
sketchband <65875934+sketchband@users.noreply.github.com>
2020-06-20 18:11:44 +0900

    1

---
+++ BoardMvc7Ora/src/my/action7/CommandAction7.java
@@ -0,0 +1,10 @@
+package my.action7;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface CommandAction7 {
+
+	public String RequestPro(HttpServletRequest request, HttpServletResponse response);
+	
+}
